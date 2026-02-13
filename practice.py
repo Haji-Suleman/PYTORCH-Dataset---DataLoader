3a6ccc92cd00fb3b0ad1d570b3ad541f991c173b
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-02-13 21:36:39 +0500

    Adding Practices of HEart Datasest

--- practice.py
+++ practice.py
@@ -7,7 +7,14 @@ import math
 
 class HeartDataSet:
     def __init__(self):
+
+        # Loading Csv files using numpy
         data = np.loadtxt("heart.csv", delimiter=",", dtype=np.float32, skiprows=1)
+
+        # Class Labels
         self.X = torch.from_numpy(data[:, :13])
         self.y = torch.from_numpy(data[:[13]])
         self.n_samples = data.shape[0]
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
