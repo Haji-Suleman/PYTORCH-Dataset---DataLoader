13407759333281b8c49b0e067be561af37132c86
Haji Suleman <mirhajisulemanjamali@gmail.com>
2026-02-13 21:52:54 +0500

    Adding Dataloader

--- practice.py
+++ practice.py
@@ -9,11 +9,10 @@ class HeartDataSet:
     def __init__(self):
 
         # Loading Csv files using numpy
-        data = np.loadtxt("heart.csv", delimiter=",", dtype=np.float32, skiprows=1)
-
+        data = np.loadtxt("heart.csv", delimiter=",", skiprows=1)
         # Class Labels
         self.X = torch.from_numpy(data[:, :13])
-        self.y = torch.from_numpy(data[:[13]])
+        self.y = torch.from_numpy(data[:, [13]])
         self.n_samples = data.shape[0]
 
     def __getitem__(self, index):
@@ -29,4 +28,12 @@ firstData = dataset[0]
 
 features, labels = firstData
 
-print(firstData)
+
+dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
+
+total_samples = len(dataloader)
+
+n_iteration = total_samples // 4
+
+print(total_samples, n_iteration)
+
